53bdfb509a35eaeb1c3641cf34a2cdf6e5b4100e
Michael Kenneth Rasch <miketriticum@gmail.com>
2019-09-17 11:30:42 +0200

    Add files via upload

--- ci.js
+++ ci.js
@@ -1,40 +1,25 @@
 (function main(what) {
 
   const cinemaList = async (what) => {
-    const res = await axios.get('http://www.omdbapi.com' + what + '&apikey=.......')
+    const res = await axios.get('http://www.omdbapi.com' + what + '&apikey=9022e30')
     return res.data;
   }
 
   cinemaList(what).then((data) => {
     data.Search.map((slot) => {
-      var o = filmBoxItem(slot)
-      for (var pair in slot) {
-        o.appendChild(insertElement(key = pair, slot[pair]))
-      }
-      zdata.appendChild(o)
+
+      zcopy.childNodes[1].childNodes[1].textContent = slot.Title
+      zcopy.childNodes[1].childNodes[3].src = slot.Poster            
+      zcopy.childNodes[1].childNodes[5].childNodes[1].textContent = slot.Year            
+      zcopy.childNodes[1].childNodes[7].childNodes[1].textContent = slot.Type      
+
+      div = document.createElement("div")
+      div.innerHTML = zcopy.childNodes[1].outerHTML;
+      zdata.appendChild(div)
+
     })
   })
 
-  function insertElement(key, slot) {
-    i = document.createElement("div")
-    i.setAttribute("class", 'line')
-    l = document.createElement("p")
-    l.setAttribute("class", 'lead')
-    l.textContent = key
-    r = document.createElement("p")
-    r.setAttribute("class", 'value')
-    r.textContent = slot
-    i.appendChild(l)
-    i.appendChild(r)
-    return i
-  }
-  function filmBoxItem(slot) {
-    o = document.createElement("div")
-    o.setAttribute('class', 'filmBox')
-    b = document.createElement('img')
-    b.setAttribute('src', slot.Poster)
-    o.appendChild(b)
-    return o
-  }
+
 }
 )(window.location.search)
